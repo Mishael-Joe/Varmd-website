47b4a7eba854a23d43d89e7a1bdfbd6596a5221d
mishael joseph <mishaeljoe55@gmail.com>
2023-10-07 20:16:40 +0100

    added a new page named about-us

---
+++ components/aboutUs/Certification_AboutUs.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Certification_AboutUs() {
+  return (
+    <section>
+        <div className='flex items-center container mx-auto px-4 sm:px-8 py-6'>
+            <div className=' basis-3/7'>
+                <Image 
+                src={'/aboutUs/undefined38.png'}
+                width={400}
+                height={100}
+                alt=''
+                quality={100}
+                className=' object-cover w-[80%]'
+                />
+            </div>
+
+            <div className='basis-4/7 max-w-2xl'>
+                <p className=' text-4xl font-bold pb-4'>ISO 9001:2015</p>
+                <p className='text-slate-600'>We are ISO certified and provide a wide range of services that includes quality assurance, supplier audits, product sourcing and product development. Our team is dedicated to help you get the best value for your money and ensure that you get the highest quality products.</p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Certification_AboutUs
---
+++ components/aboutUs/index.ts
@@ -0,0 +1,11 @@
+import Header_AboutUs from "./Header_AboutUs";
+import IntroVideo_AboutUs from "./IntroVideo_AboutUs";
+import Certification_AboutUs from "./Certification_AboutUs";
+import CoreValue_AboutUs from "./CoreValue_AboutUs";
+
+export {
+    Header_AboutUs,
+    IntroVideo_AboutUs,
+    Certification_AboutUs,
+    CoreValue_AboutUs,
+}
